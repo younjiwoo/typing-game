c0e2dbe48d9197c566533dc0867141890032332f
Younji Woo <younjiwoo@gmail.com>
2020-09-06 19:15:17 +0900

    added react routers and /game page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Game from './Game';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/game" component={Game} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
